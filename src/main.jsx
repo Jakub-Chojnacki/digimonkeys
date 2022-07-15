fd25be5bf29f06acf8c48ffe3c30be32d2b8633e
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-07-15 09:49:30 +0200

    changed the font to Kumbh Sans

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { VideoProvider } from './context/video-context'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const fonts = {
+  body: `'Kumbh Sans', sans-serif`,
+
+}
+
+const theme = extendTheme({fonts})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <VideoProvider>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </VideoProvider>
